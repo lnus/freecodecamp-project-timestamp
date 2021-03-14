41e7adcf46a3b26d5bdc7593149fd2fdc2dd5ab8
linus <linus.wahler@gmail.com>
2021-03-14 01:52:11 +0100

    Completed the challenge

--- server.js
+++ server.js
@@ -8,25 +8,39 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+// creates and mounts a logger function to see where requests are coming from
+const LOGGER = (req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+}
+
+app.use(LOGGER);
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/timestamp/:date", (req, res) => {
+  let date = req.params["date"];
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  // if the input isn't already in unix format, convert it.
+  let unixTimestamp = (isNaN(date)) ? Date.parse(date) : parseInt(date);
+
+  // convert it into readable data using .toDateString()
+  let localeDate = new Date(unixTimestamp).toUTCString();
+  console.log(localeDate);
+
+  // returns the unix timestamp and the utc date
+  res.json({unix: unixTimestamp, utc: localeDate});
 });
 
-
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 5000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
